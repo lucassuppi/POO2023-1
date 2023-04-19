df94b167c508eb706a6c5e5fac2456c376fdf421
Lucas Suppi <88332280+lucassuppi@users.noreply.github.com>
2023-04-18 21:06:35 -0300

    Add files via upload

---
+++ Grau A/Prova/Biomas.py
@@ -0,0 +1,25 @@
+faunaBR = [
+    # [Animal	Amazônia Mata_Atlântica	Cerrado	Caatinga Pampa	Pantanal]
+    ['Capivara',	True,	True,	True,	True,	True,	True],
+    ['Gralha azul',	False,	True,	False,	False,	True,	False],
+    ['Tamanduá-bandeira',	True,	True,	True,	False,	True,	False],
+    ['Onça pintada',	True,	True,	False,	True,	False,	True],
+    ['Tatu-bola',	False,	False,	False,	True,	False,	False]
+]
+
+floraBR = [
+    # [Planta	Amazônia Mata_Atlântica	Cerrado	Caatinga Pampa	Pantanal]
+    ['Ipê amarelo',	True,	True,	True,	True,	True,	True],
+    ['Araucária',	False,	True,	False,	False,	True,	False],
+    ['Mandacaru',	False,	False,	True,	True,	False,	False],
+    ['Vitória-régia',	True,	False,	False,	False,	False,	True],
+    ['Jatobá',	True,	True,	True,	False,	False,	True]
+
+]
+
+# Diquinha de como acessar a tabela ;)
+# for i in range(len(faunaBR)):
+#    for j in range(len(faunaBR[i])):
+#        print(faunaBR[i][j], end=' ')
+#    print('\n')
+

f1bc794fa14b470c4331cab4f88b1f4547231065
Lucas Suppi <88332280+lucassuppi@users.noreply.github.com>
2023-05-08 17:05:56 -0300

    Delete Grau A/Trabalho Grau A directory

--- Grau A/Trabalho Grau A/objects/album.py
+++
@@ -1,5 +0,0 @@
-class Album:
-    def __init__(self, pages, stickers, exchangerequests):
-        self.__pages = pages
-        self.__stickers = stickers
-        self.__exchangerequests = exchangerequests
--- Grau A/Trabalho Grau A/objects/page.py
+++
@@ -1,9 +0,0 @@
-class Page:
-    def __init__(self, title, min_number, max_number):
-        self.__sticker = []
-        self.__title = title
-        self.__min_number = min_number
-        self.__max_number = max_number
-
-    def add_sticker(self, sticker):
-        self.__sticker.append(sticker)
